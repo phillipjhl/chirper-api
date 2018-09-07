2ee03493f93724c18695b31b4773c547eaabe77a
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-07 12:42:19 -0500

    added get and post routes

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,10 +1,32 @@
+//module imports
 const express = require('express');
-const chirpsstore = require('../chirpstore');
+const chirpsStore = require('../chirpstore');
 
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('chirps');
+//get router
+//if it has id, respond with one 
+//else send all to client
+router.get('/:id?', (req, res) => {
+    let id = req.params.id;
+    if (id) {
+        res.json(chirpsStore.GetChirp(id));
+    } else {
+        res.send(chirpsStore.GetChirps())
+    };
 });
+
+//post router
+//save resource sent from client
+router.post('/', (req, res) => {
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200);
+});
+
+//put router
+//update resource with data
+
+//delete router
+//delete resource with id
 
 module.exports = router;
