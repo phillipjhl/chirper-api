28f772d8e953ac3aeafc3bd1f70ea35727f88163
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-07 12:41:55 -0500

    added comments added express.static for client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+//Main server for app
+
+//Module imports
+//Import index.js for main router
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./routes');
 
 let app = express();
 
+//Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('../client'));
 
 app.use('/api', apiRoutes);
 
